109132a9597e8d572929a12e00d9d914343261f7
reddynew <hanumanreddyy@gmail.com>
2026-04-02 16:00:07 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="flex justify-center items-center h-screen">CRM APPLICATION</h1>
+      <h1 className="flex justify-center items-center h-screen">CRM APPLICATION Maanvi Digitals</h1>
     </div>
   );
 }
